acb0fb2ee2d78b0b8a67b0e61f0bf66378c3d177
Wang1063267665 <w18434803813@163.com>
2018-04-12 11:47:10 +0800

    commit

--- springboot-01-helloworld/src/main/java/com/helloworld/HelloWorldMainApplication.java
+++ springboot-01-helloworld/src/main/java/com/helloworld/HelloWorldMainApplication.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *  @SpringBootApplication 标识是springboot的启动类
+ *  @SpringBootApplication 来标注一个主程序类，说明这是个springboot应用
  */
 @SpringBootApplication
 public class HelloWorldMainApplication {
 
 
     public static void main(String[] args) {
-
+        //spring应用启动起来
         SpringApplication.run(HelloWorldMainApplication.class,args);
 
 
