a31c217eb26e5dbf1fcd2f2d7816f255dd668756
Wang1063267665 <w18434803813@163.com>
2018-04-12 17:21:39 +0800

    commit

---
+++ springboot-02-autoconfig/src/main/java/com/springboot/Springboot02AutoconfigApplication.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02AutoconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot02AutoconfigApplication.class, args);
+	}
+}
