005509cbbd879e87312dbd260392dd4ab360ef31
Wang1063267665 <w18434803813@163.com>
2018-04-16 14:43:11 +0800

    commit

--- springboot-04-web-restfulcrud/src/main/java/com/springboot/commponent/MyLocaleResolver.java
+++ springboot-04-web-restfulcrud/src/main/java/com/springboot/commponent/MyLocaleResolver.java
@@ -17,7 +17,7 @@ public class MyLocaleResolver implements LocaleResolver {
     @Override
     public Locale resolveLocale(HttpServletRequest httpServletRequest) {
         String l = httpServletRequest.getParameter("l");
-        Locale locale = null;
+        Locale locale = Locale.ROOT;
         if (!StringUtils.isEmpty(l)){
             String[] split = l.split("_");
             locale =  new Locale(split[0],split[1]);
